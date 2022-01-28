f1be3e3c0e03af550438dfc9e57c313a37f58653
JoaoZati <joaozati@gmail.com>
2022-01-28 14:02:49 -0300

    Add Register Resource

--- web/app.py
+++ web/app.py
@@ -5,12 +5,13 @@ Sotore a sentece for 1 token
 Retrive his stored sentence on our database for 1 token
 """
 
-from flask import Flask
-from flask_restful import Api
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
 
 from pymongo import MongoClient
 from debugger import initialize_debugger
 
+import bcrypt
 
 app = Flask(__name__)
 api = Api(app)
@@ -24,6 +25,72 @@ users = db["Users"]
 def hello_word():
     return 'Hello Word'
 
+
+def get_data():
+    status_code = 200
+    message = "Ok"
+
+    try:
+        post_data = request.get_json()
+
+        username = post_data["username"]
+        password = post_data["password"]
+    except Exception as e:
+        status_code = 305
+        message = str(e)
+        username, password = [0]*2
+    
+    return status_code, message, username, password
+
+
+def user_already_exist(username):
+    try:
+        if users.find({}, {"Username": username})[0]['Username'] == username:
+            return True
+    except Exception as e:
+        print(e)
+    
+    return False
+
+class Register(Resource):
+    def post(self):
+        status_code, message, username, password = get_data()
+
+        if status_code != 200:
+            return jsonify(
+                {
+                    'Status Code': status_code,
+                    'Message': message,
+                }
+            )
+        
+        if user_already_exist(username):
+            return jsonify(
+                {
+                    'Status Code': 301,
+                    'Message': "User already exists",
+                }
+            )
+
+        hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+        users.insert_one(
+            {
+                "Username": username,
+                "Password": hashed_password,
+                "Tokens": 5
+            }
+        )
+
+        return jsonify(
+            {
+                'Status Code': status_code,
+                'Message': message,
+            }
+        )
+
+
+api.add_resource(Register, "/register")
 
 if __name__ == '__main__':
     initialize_debugger()
